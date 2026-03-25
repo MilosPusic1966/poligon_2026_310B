6d4066b788c855b527bd2a43c1e033c8b44f084f
milos pusic <milos.pusic@prvabeogim.edu.rs>
2026-03-25 19:20:44 +0100

    Add project files.

---
+++ poligon.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poligon_2026_310B
+{
+    class poligon
+    {
+        int br_temena;
+        tacka[] teme;
+        public poligon(int n) 
+        { 
+            br_temena= n;
+            teme = new tacka[n];
+        }
+        public static poligon unos()
+        {
+            Console.WriteLine("Unesite broj temena=");
+            int br=Convert.ToInt32(Console.ReadLine());
+            poligon novi = new poligon(br);
+            for (int i = 0; i < br; i++)
+            {
+                Console.Write("Teme({0}).x=", i+1);
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Teme({0}).y=", i + 1);
+                double y = Convert.ToDouble(Console.ReadLine());
+                novi.teme[i]=new tacka(x, y);
+            }
+            return novi;
+        }
+        public void stampa()
+        {
+            Console.WriteLine("Poligon sa "+br_temena+" temena:");
+            for (int i = 0; i < br_temena; i++)
+            {
+                Console.WriteLine("x={0} y={1}", teme[i].x, teme[i].y);
+            }
+        }
+
+    }
+}
---
+++ vektor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poligon_2026_310B
+{
+    internal class vektor
+    {
+        public tacka pocetak;
+        public tacka kraj;
+        public tacka centriraj()
+        {
+            double x = kraj.x - pocetak.x;
+            double y = kraj.y - pocetak.y;
+            return new tacka(x, y);
+        }
+        public void stampaj()
+        {
+            Console.WriteLine("Od x1={0}, y1={1} Do x2={2}, y2={3}", pocetak.x, pocetak.y, kraj.x, kraj.y);
+        }
+        public vektor(tacka a, tacka b)
+        {
+            pocetak = a;
+            kraj = b;
+        }
+    }
+}
