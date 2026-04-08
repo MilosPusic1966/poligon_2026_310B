f6d16aecfad6ea5b74456a60e3fec402bc60efa7
milos pusic <milos.pusic@prvabeogim.edu.rs>
2026-04-08 18:59:02 +0200

    konveksan

--- poligon.cs
+++ poligon.cs
@@ -99,5 +99,22 @@ namespace poligon_2026_310B
             }
             return true;
         }
+        public bool konveksan()
+        {
+            int t = 0;
+            for (int i = 0; i < br_temena; i++)
+            {
+                vektor prvi = new vektor(teme[i], teme[(i+1)%br_temena]);
+                vektor drugi = new vektor(teme[(i+1)%br_temena], teme[(i+2)%br_temena]);
+                if (vektor.VP(prvi, drugi) > 0) t++;
+            }
+            if ((t == br_temena) || (t == 0)) return true;
+            return false;
+        }
+        public double povrsina()
+        {
+            return 0;
+        }
+
     }
 }
