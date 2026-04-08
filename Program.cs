5d980dfb41077a817f1266d6fe1423ae85917a50
milos pusic <milos.pusic@prvabeogim.edu.rs>
2026-04-08 19:04:15 +0200

    konveksan test

--- Program.cs
+++ Program.cs
@@ -18,19 +18,23 @@ namespace poligon_2026_310B
             vektor AB = new vektor(A, B);
             AB.stampaj();
             */
-            /*poligon prvi = poligon.unos();
-            prvi.stampa();
-            prvi.snimi();
-            */
-            // poligon drugi = poligon.ucitaj();
-            // drugi.stampa();
-            tacka A = new tacka(1, 1);
-            tacka B = new tacka(5, 1);
-            tacka C = new tacka(2, 0.1);
-            tacka D = new tacka(2, 0);
-            vektor AB = new vektor(A, B);
-            vektor CD = new vektor(C, D);
-            Console.WriteLine(AB.sece(CD));
+/*poligon prvi = poligon.unos();
+prvi.stampa();
+prvi.snimi();
+*/
+// poligon drugi = poligon.ucitaj();
+// drugi.stampa();
+/*
+    tacka A = new tacka(1, 1);
+    tacka B = new tacka(5, 1);
+    tacka C = new tacka(2, 0.1);
+    tacka D = new tacka(2, 0);
+    vektor AB = new vektor(A, B);
+    vektor CD = new vektor(C, D);
+    Console.WriteLine(AB.sece(CD));
+       */
+        poligon prvi = poligon.unos();
+        Console.WriteLine(prvi.konveksan());
         }
     }
 }
